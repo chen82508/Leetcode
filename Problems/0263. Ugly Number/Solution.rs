e9c9dfce70047e5a1b550544cd5650d0f2ea130a
chen82508 <chen82508@gmail.com>
2025-05-03 23:29:47 +0800

    Add solutions of problem 263

---
+++ Problems/0263. Ugly Number/Solution.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn is_ugly(n: i32) -> bool {
+        let mut num: i32 = n;
+        if num <= 0 {
+            return false;
+        }
+
+        while num % 2 == 0 {
+            num /= 2;
+        }
+        while num % 3 == 0 {
+            num /= 3;
+        }
+        while num % 5 == 0 {
+            num /= 5;
+        }
+
+        return num == 1;
+    }
+}
